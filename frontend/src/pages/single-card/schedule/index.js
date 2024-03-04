c00e51a5b3332c342b25ccce4b3e837b4983a2f7
Wiltort <wiltort21@gmail.com>
2024-03-04 23:16:40 +0500

    ываы

--- frontend/src/pages/single-card/schedule/index.js
+++ frontend/src/pages/single-card/schedule/index.js
@@ -1,12 +1,12 @@
 import styles from './styles.module.css'
 
 const Schedule = ({ items }) => {
-  const isEmpty = true
-  for (item in items){
+  let isEmpty = true
+  for (let key in items){
     isEmpty = false
   }
   if (isEmpty) { return null }
-  else {return <div className={styles.ingredients}>
+ {return <div className={styles.ingredients}>
     <h3 className={styles['ingredients__title']}>Расписание:</h3>
     <div className={styles['ingredients__list']}>
       {items[0].items.map(({
