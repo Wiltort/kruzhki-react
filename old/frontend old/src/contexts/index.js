9fc363c42b220360c374192b5b6baa124c768310
Wiltort <wiltort21@gmail.com>
2024-03-05 16:34:51 +0500

    34

--- old/frontend old/src/contexts/index.js
+++
@@ -1,9 +0,0 @@
-import GroupsContext from './Groups-context'
-import AuthContext from './auth-context'
-import UserContext from './user-context'
-
-export {
-  GroupsContext,
-  AuthContext,
-  UserContext
-}
